2a98c29411c6b4ba0ecaf885d8c2ae64036e100d
Chris Parent <chrisnparent@gmail.com>
2019-12-08 13:58:27 -0500

    day 8 part 2

--- year2019/t08/p2.py
+++ year2019/t08/p2.py
@@ -5,6 +5,9 @@ class Row:
     def countOf(self, c):
         return self.data.count(c)
 
+    def show(self):
+        return self.data.replace('0', ' ').replace('1','*').replace('2', ' ')
+
 class Layer:
     def __init__(self, w, h, data):
         self.w = w
@@ -16,8 +19,6 @@ class Layer:
             e = s + self.w
             self.rows.append(Row(data[s:e]))
             i += 1
-        
-        print('\n')
 
     def countOf(self, c):
         i = 0
@@ -27,6 +28,10 @@ class Layer:
             i += 1
 
         return total
+
+    def show(self):
+        rows = list(map(lambda x: x.show(), self.rows))
+        return '\n'.join(rows)
 
 class Image:
     def __init__(self, w, h, data):
@@ -43,18 +48,27 @@ class Image:
             self.layers.append(Layer(w, h, data[s:e]))
             i += 1
 
+    def render(self):
+        data = []
+        i = 0
+        while i < self.h:
+            j = 0
+            while j < self.w:
+                data.append(self.renderAt(j, i))
+                j += 1
+            i += 1
+
+        return Layer(self.w, self.h, ''.join(data))
+
+    def renderAt(self, x, y):
+        n = len(self.layers)
+        i = 0
+        while i < n:
+            val = self.layers[i].rows[y].data[x]
+            if val != '2': return val
+            i += 1
+
 def run(args):
     data = args[0]
     img = Image(25,6,data)
-    i = 0
-    counts = []
-    while i < len(img.layers):
-        counts.append(img.layers[i].countOf('0'))
-        i += 1
-
-    minCount = min(counts)
-    i = counts.index(minCount)
-
-    ones = img.layers[i].countOf('1')
-    twos = img.layers[i].countOf('2')
-    return ones * twos
+    return img.render().show()
