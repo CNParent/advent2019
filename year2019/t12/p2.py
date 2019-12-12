1c545cc3b201feb3608d9036dc99ae9887b95cc2
Chris Parent <chrisnparent@gmail.com>
2019-12-12 17:56:51 -0500

    equality checking

--- year2019/t12/p2.py
+++ year2019/t12/p2.py
@@ -1,22 +1,25 @@
+import sys
+import time
+
 class Point:
 	def __init__(self, x = 0, y = 0, z = 0):
 		self.x = x
 		self.y = y
 		self.z = z
 		
+	def __eq__(self, other):
+		return self.x == other.x and self.y == other.y and self.z == other.z
+
 	def getMagnitude(self):
 		return abs(self.x) + abs(self.y) + abs(self.z)
-
-	def fastHash(self):
-		return self.x * 10000 + self.y * 100 + self.z
-
-	def slowHash(self):
-		return f'{self.x}{self.y}{self.z}'
 
 class Body:
 	def __init__(self, pos, vel):
 		self.pos = pos
 		self.vel = vel
+
+	def __eq__(self, other):
+		return self.pos == other.pos
 		
 	def gravitate(self, other):
 		dx = other.pos.x - self.pos.x
@@ -38,15 +41,12 @@ class Body:
 	def getEnergy(self):
 		return self.pos.getMagnitude() * self.vel.getMagnitude()
 
-	def fastHash(self):
-		return self.pos.fastHash() * 10000 + self.vel.fastHash()
-
-	def slowHash(self):
-		return f'{self.pos.slowHash()}{self.vel.slowHash()}'
-
 class System:
 	def __init__(self, bodies):
 		self.bodies = bodies
+
+	def __eq__(self, other):
+		return all(self.bodies[i] == other.bodies[i] for i in range(0, len(self.bodies)))
 		
 	def step(self):
 		for i in range(0, len(self.bodies)):
@@ -58,30 +58,27 @@ class System:
 	def getEnergy(self):
 		return sum(b.getEnergy() for b in self.bodies)
 
-	def fastHash(self):
-		return sum(b.fastHash() for b in self.bodies)
-
-	def slowHash(self):
-		return ''.join(b.slowHash() for b in self.bodies)
+	def equals(self, other):
+		n = len(self.bodies)
+		if any(self.bodies[i] != other.bodies[i] for i in range(0, n)): return False
+		else: return all(self.bodies[i] == other.bodies[i] for i in range(0, n))
 		
 def parseBody(text):
 	text = text.replace('>', '').replace('<', '').replace(' ','')
 	pointsTxt = text.split(',')
-	x = int(pointsTxt[0].split('=')[1])
-	y = int(pointsTxt[1].split('=')[1])
-	z = int(pointsTxt[2].split('=')[1])
+	x = float(pointsTxt[0].split('=')[1])
+	y = float(pointsTxt[1].split('=')[1])
+	z = float(pointsTxt[2].split('=')[1])
 	return Body(Point(x, y, z), Point(0, 0, 0))
-	
-def run(args):
-	bodies = list(parseBody(text) for text in args)	
-	s = System(bodies)
-	fast = set()
-	slow = set()
-	while True:
-		if s.fastHash() in fast and s.slowHash in slow: return len(cfg)
-		if len(fast) > 4686774924: return 'Error'
-		if len(fast) % 10000 == 0: print(f'Attempt {len(cfg)}')
 
-		fast.add(s.fastHash())
-		slow.add(s.slowHash())
+def run(args):
+	o = System(list(parseBody(text) for text in args))
+	s = System(list(parseBody(text) for text in args))
+	n = 0
+	ts = time.time()
+	while True:
+		n += 1
 		s.step()
+		if s == o: return n
+		if n > 1000000: return f'Process took {time.time() - ts} to reach 1 000 000 iterations'
+		if n % 10000 == 0: print(f'{n} of {4686774924}', end='\r', flush=True)
