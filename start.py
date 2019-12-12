fc236735b6f990688f467394bf6151c12624eb0d
Chris Parent <chrisnparent@gmail.com>
2019-12-11 19:15:55 -0500

    day 11 part 1

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
 import lib.loadinput
 import config
-import year2019.t10.p2 as task
+import year2019.t11.p1 as task
 
 year = 2019
-day = 10
+day = 11
 
 args = lib.loadinput.get(day, year, config.sessionValue)
 print(task.run(args))
---
+++ year2019/t11/p1.py
@@ -0,0 +1,167 @@
+import copy
+
+class Program:
+    def __init__(self, source = []):
+        self.source = source
+        self.inputs = []
+        self.outputs = []
+        self.end = False
+        self.i = 0
+        self.b = 0
+
+    def run(self):
+        self.outputs = []
+        while True:
+            opcode = self.source[self.i] % 100
+            if opcode == 1: self.add()
+            if opcode == 2: self.multiply()
+            if opcode == 3:
+                if len(self.inputs) == 0: return 
+                self.input()
+            if opcode == 4: self.output()
+            if opcode == 5: self.jumpIfTrue()
+            if opcode == 6: self.jumpIfFalse()
+            if opcode == 7: self.lessThan()
+            if opcode == 8: self.equals()
+            if opcode == 9: self.relativeBase()
+            if opcode == 99: 
+                self.end = True
+                return
+
+    def getModes(self, n):
+        opcode = str(self.source[self.i]).zfill(n)[0:n-2]
+        modes = list(map(lambda x: int(x), opcode))
+        modes.reverse()
+        return modes
+
+    def move(self, n):
+        self.i = self.i + n
+
+    def getAddresses(self, n):
+        modes = self.getModes(n + 2)
+        for m in range(0, n):
+            i = self.i + m + 1
+            if modes[m] == 1: yield i
+            elif modes[m] == 2: yield self.getAt(i) + self.b
+            else: yield self.getAt(i)
+
+    def getValues(self, addresses):
+        for address in addresses:
+            yield self.getAt(address)
+
+    def getAt(self, i):
+        self.grow(i)
+        return self.source[i]
+
+    def setAt(self, i, v):
+        self.grow(i)
+        self.source[i] = v
+
+    def grow(self, i):
+        n = i - len(self.source) + 1
+        if n <= 0: return
+        self.source += list(0 for x in range(0, n))
+
+    def add(self):
+        a = list(self.getAddresses(3))
+        v = list(self.getValues(a[0:2]))
+        self.setAt(a[2], v[0] + v[1])
+        self.move(4)
+
+    def multiply(self):
+        a = list(self.getAddresses(3))
+        v = list(self.getValues(a[0:2]))
+        self.setAt(a[2], v[0] * v[1])
+        self.move(4)
+
+    def input(self):
+        a = list(self.getAddresses(1))
+        self.setAt(a[0], self.inputs.pop(0))
+        self.move(2)
+
+    def output(self):
+        a = list(self.getAddresses(1))
+        v = self.getAt(a[0])
+        self.outputs.append(v)
+        self.move(2)
+
+    def jumpIfTrue(self):
+        a = list(self.getAddresses(2))
+        v = list(self.getValues(a))
+        if v[0] != 0: self.i = v[1]
+        else: self.move(3)
+
+    def jumpIfFalse(self):
+        a = list(self.getAddresses(2))
+        v = list(self.getValues(a))
+        if v[0] == 0: self.i = v[1]
+        else: self.move(3)
+        
+    def lessThan(self):
+        a = list(self.getAddresses(3))
+        v = list(self.getValues(a[0:2]))
+        self.setAt(a[2], 1 if v[0] < v[1] else 0)
+        self.move(4)
+        
+    def equals(self):
+        a = list(self.getAddresses(3))
+        v = list(self.getValues(a[0:2]))
+        self.setAt(a[2], 1 if v[0] == v[1] else 0)
+        self.move(4)
+
+    def relativeBase(self):
+        a = list(self.getAddresses(1))
+        self.b += self.getAt(a[0])
+        self.move(2)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def at(self, other):
+        return self.x == other.x and self.y == other.y
+
+class Tile:
+    def __init__(self, pos):
+        self.pos = pos
+        self.color = 0
+
+class Robot:
+    def __init__(self):
+        self.pos = Point(0, 0)
+        self.facing = 0
+
+    def turnLeft(self):
+        self.facing = (self.facing + 3) % 4
+        self.move()
+
+    def turnRight(self):
+        self.facing = (self.facing + 1) % 4
+        self.move()
+
+    def move(self):
+        if self.facing == 0: self.pos.y += 1
+        elif self.facing == 1: self.pos.x += 1
+        elif self.facing == 2: self.pos.y -= 1
+        else: self.pos.x -= 1
+
+def run(args):
+    values = list(map(lambda x: int(x), args[0].split(',')))
+    p = Program(values)
+    p.run()
+    robot = Robot()
+    tiles = []
+    while not p.end:
+        found = list(filter(lambda x: x.pos.at(robot.pos), tiles))
+        if len(found) == 0: tiles.append(Tile(copy.copy(robot.pos)))
+        
+        tile = list(filter(lambda x: x.pos.at(robot.pos), tiles))[0]
+        p.inputs = [tile.color]
+        p.run()
+        tile.color = p.outputs[0]
+        print(f'Painted tile at ({tile.pos.x},{tile.pos.y}) {"black" if tile.color == 0 else "white"}')
+        if p.outputs[1] == 0: robot.turnLeft()
+        else: robot.turnRight()
+
+    return len(tiles)
