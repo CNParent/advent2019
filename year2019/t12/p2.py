0044cba57c7f715ab08a7e7f0336cf3336dd9fe0
Chris Parent <chrisnparent@gmail.com>
2019-12-12 21:43:05 -0500

    complete rewrite

--- year2019/t12/p2.py
+++ year2019/t12/p2.py
@@ -1,84 +1,41 @@
-import sys
 import time
-
-class Point:
-	def __init__(self, x = 0, y = 0, z = 0):
-		self.x = x
-		self.y = y
-		self.z = z
-		
-	def __eq__(self, other):
-		return self.x == other.x and self.y == other.y and self.z == other.z
-
-	def getMagnitude(self):
-		return abs(self.x) + abs(self.y) + abs(self.z)
-
-class Body:
-	def __init__(self, pos, vel):
-		self.pos = pos
-		self.vel = vel
-
-	def __eq__(self, other):
-		return self.pos == other.pos
-		
-	def gravitate(self, other):
-		dx = other.pos.x - self.pos.x
-		dy = other.pos.y - self.pos.y
-		dz = other.pos.z - self.pos.z
-		if dx != 0: dx = dx / abs(dx)
-		if dy != 0: dy = dy / abs(dy)
-		if dz != 0: dz = dz / abs(dz)
-		
-		self.vel.x += dx
-		self.vel.y += dy
-		self.vel.z += dz
-		
-	def move(self):
-		self.pos.x += self.vel.x
-		self.pos.y += self.vel.y
-		self.pos.z += self.vel.z
-		
-	def getEnergy(self):
-		return self.pos.getMagnitude() * self.vel.getMagnitude()
-
-class System:
-	def __init__(self, bodies):
-		self.bodies = bodies
-
-	def __eq__(self, other):
-		return all(self.bodies[i] == other.bodies[i] for i in range(0, len(self.bodies)))
-		
-	def step(self):
-		for i in range(0, len(self.bodies)):
-			for j in range(0, len(self.bodies)):
-				self.bodies[i].gravitate(self.bodies[j])
-				
-		for b in self.bodies: b.move()
-		
-	def getEnergy(self):
-		return sum(b.getEnergy() for b in self.bodies)
-
-	def equals(self, other):
-		n = len(self.bodies)
-		if any(self.bodies[i] != other.bodies[i] for i in range(0, n)): return False
-		else: return all(self.bodies[i] == other.bodies[i] for i in range(0, n))
+from itertools import product
 		
 def parseBody(text):
 	text = text.replace('>', '').replace('<', '').replace(' ','')
 	pointsTxt = text.split(',')
-	x = float(pointsTxt[0].split('=')[1])
-	y = float(pointsTxt[1].split('=')[1])
-	z = float(pointsTxt[2].split('=')[1])
-	return Body(Point(x, y, z), Point(0, 0, 0))
+	x = int(pointsTxt[0].split('=')[1])
+	y = int(pointsTxt[1].split('=')[1])
+	z = int(pointsTxt[2].split('=')[1])
+	return [x, y, z, 0, 0, 0]
+
+def delta(a, b):
+	if a == b: return 0
+	elif a < b: return 1
+	else: return -1
 
 def run(args):
-	o = System(list(parseBody(text) for text in args))
-	s = System(list(parseBody(text) for text in args))
+	o = list(parseBody(text) for text in args)
+	s = list(parseBody(text) for text in args)
 	n = 0
+	tgrav = 0
+	tmove = 0
+	tcomp = 0
 	ts = time.time()
 	while True:
 		n += 1
-		s.step()
-		if s == o: return n
-		if n > 1000000: return f'Process took {time.time() - ts} to reach 1 000 000 iterations'
-		if n % 10000 == 0: print(f'{n} of {4686774924}', end='\r', flush=True)
+		then = time.time()	
+		for a, b, i in product(s, s, range(0, 3)):
+			a[i + 3] += delta(a[i], b[i])
+		tgrav += time.time() - then
+
+		then = time.time()
+		for a, i in product(s, range(0, 3)):
+			a[i] += a[i + 3]
+		tmove += time.time() - then
+
+		then = time.time()
+		if all(o[i][j] == s[i][j] for i, j in product(range(0, 4), range(0, 6))): return n
+		if n > 1000000: return f'Process took {time.time() - ts} to reach 1 000 000 iterations. tgrav={tgrav} tmove={tmove} tcomp={tcomp}'
+		if n % 1000 == 0: print(f'{n} of {4686774924}', end='\r', flush=True)
+		tcomp += time.time() - then
